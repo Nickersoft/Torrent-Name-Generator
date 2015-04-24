4f5411e1022ba226b1b0e8f069a67797bb2db0da
Tyler Nickerson <nickersoft@gmail.com>
2015-04-23 21:37:20 -0400

    Updated Kickass Torrents URL

--- get_torrents.php
+++ get_torrents.php
@@ -1,7 +1,7 @@
 <?php
 
 //Get the homepage HTML for Kickass Torrents
-$html = file_get_contents('http://kickass.to/');
+$html = file_get_contents('http://kickasstorrents.im/');
 echo gzdecode($html);
 
 ?>
